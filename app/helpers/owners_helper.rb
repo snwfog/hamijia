f24439f842ae0bccbe13932941caf647d1f8b373
snwfog <donchoa@gmail.com>
2014-11-13 22:47:39 -0500

    Fix the form lol because didnt put the equal sign at the form builder

--- app/helpers/owners_helper.rb
+++ app/helpers/owners_helper.rb
@@ -4,11 +4,5 @@ module OwnersHelper
       o.pets.build if o.pets.empty?
       o.children.build if o.children.empty?
     end
-
-    pr "I love charles"
-    pr owner.pets.count
-    pr owner.children.count
-
-    owner
   end
 end
